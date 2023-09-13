7f08628415aebc13b0a95a295fccd9394a23ef67
Nat Weerawan <nat.wrw@gmail.com>
2023-09-13 10:25:59 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+echo "--------------- 1 ---------------"
+env | sort -n
 # Split the string based on "@" symbol and take the first part
 TEMP_PART=$(echo "$GITHUB_WORKFLOW_REF" | awk -F "@" '{print $1}')
 
@@ -8,4 +10,5 @@ TEMP_PART=$(echo "$GITHUB_WORKFLOW_REF" | awk -F "@" '{print $1}')
 WORKFLOW_FILE_PATH=$(echo "$TEMP_PART" | awk -F "/" '{for (i=4; i<=NF; ++i) printf $i (i==NF ? ORS : "/")}')
 echo "WORKFLOW_FILE_PATH=$WORKFLOW_FILE_PATH" >> $GITHUB_ENV
 
+echo "--------------- 2 ---------------"
 env | sort -n
