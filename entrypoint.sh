f2acc3e54164cc01152e9e3099b501a603d4bea9
Nat Weerawan <nat.wrw@gmail.com>
2023-09-13 10:29:19 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,12 @@ env | sort -n
 TEMP_PART=$(echo "$GITHUB_WORKFLOW_REF" | awk -F "@" '{print $1}')
 
 # Remove the prefix up to the last slash to get only the file path
-WORKFLOW_FILE_PATH=$(echo "$TEMP_PART" | awk -F "/" '{for (i=4; i<=NF; ++i) printf $i (i==NF ? ORS : "/")}')
+WORKFLOW_FILE_PATH=$(echo "$TEMP_PART" | awk -F "/" '{for (i=3; i<=NF; ++i) printf $i (i==NF ? ORS : "/")}')
+WORKFLOW_FILE_NAME=$(echo "$TEMP_PART" | awk -F "/" '{for (i=5; i<=NF; ++i) printf $i (i==NF ? ORS : "/")}')
+
 echo "WORKFLOW_FILE_PATH=$WORKFLOW_FILE_PATH" >> $GITHUB_ENV
+echo "WORKFLOW_FILE_NAME=$WORKFLOW_FILE_NAME" >> $GITHUB_ENV
+
 
 echo "--------------- 2 ---------------"
 env | sort -n
